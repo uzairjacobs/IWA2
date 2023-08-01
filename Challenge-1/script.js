a575b587ff22fc71979bbe41f49fd23d717e7471
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-08-01 02:07:49 +0200

    completed challenge 1

---
+++ Challenge-1/script.js
@@ -0,0 +1,11 @@
+/* This is the firstname of the user */
+
+const user = 'John';
+
+
+/* This is the lastname of the user */
+  const surname = 'Smith';
+
+
+console.log(user);
+console.log(surname);
